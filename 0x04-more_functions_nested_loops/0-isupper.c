bbf3def7faddffaaab8382d9d7a730417ec61bf0
RababMosad <rababmosad65@example.com>
2023-03-23 08:55:52 -0700

    tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,10 +7,8 @@
  *     */
 int _isupper(int c)
 {
-
-       if (x >= 65 && x <= 90)
-       {
-       return (1);
-       }
-       return (0);
+		if (c >= 'A' && c <= 'Z')
+					return (1);
+			else
+						return (0);
 }
