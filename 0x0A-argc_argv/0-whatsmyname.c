0d08cbbec9cebb052d6971256ed03443e111e803
RababMosad <rababmosad65@example.com>
2023-04-10 05:28:25 -0700

    task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
-{
-	printf("%s\n", *argv);
 
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
